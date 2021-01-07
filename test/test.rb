d999b48008a66f488b7b900a383be23272068147
Sourav Goswami <souravgoswami@protonmail.com>
2021-01-08 01:24:18 +0530

    Updated wordlist

--- test/test.rb
+++ test/test.rb
@@ -1,7 +1,7 @@
 require "bundler/setup"
 require "string_dot_gradient"
 
-words = IO.readlines(File.join(__dir__, 'words')).reject! { |x| x.length < 6 }.join.freeze
+words = IO.readlines(File.join(__dir__, 'words')).join.freeze
 
 t = Time.now
 puts words.gradient('f55', '55f', '#3eb', '#55f')
