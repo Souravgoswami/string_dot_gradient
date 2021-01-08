5aa7eb3d063c9cc437cd3c33db0d828b8b13571e
Sourav Goswami <souravgoswami@protonmail.com>
2021-01-08 11:48:55 +0530

    Updated test colours

--- test/test.rb
+++ test/test.rb
@@ -4,5 +4,5 @@ require "string_dot_gradient"
 words = IO.readlines(File.join(__dir__, 'words')).join.freeze
 
 t = Time.now
-puts words.gradient('f55', '55f', '#3eb', '#55f')
+puts words.gradient('55f', '#3eb', '#55f', 'FEC40E', 'E93884')
 puts "Time: #{Time.now - t}"
