277b93835679fc0c2711a32f9832b3a930a40149
Sourav Goswami <souravgoswami@protonmail.com>
2021-06-25 19:58:32 +0530

    Require hex_to_rgb file in main file

--- lib/string_dot_gradient.rb
+++ lib/string_dot_gradient.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "string_dot_gradient/version"
+require_relative 'string_dot_gradient/version'
+require 'string_dot_gradient/hex_to_rgb.rb'
 require 'string_dot_gradient/gradient.rb'
